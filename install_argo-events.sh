3ef294fe6f7d3e2693c12d6a16207e0a6a9da2da
Thomas Labarussias <issif_github@gadz.org>
2021-05-09 23:26:02 +0200

    split folders into 2
    Signed-off-by: Thomas Labarussias <issif_github@gadz.org>

--- install_argo-events.sh
+++ install_argo-events.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+kubectl create namespace argo-events
+kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/install.yaml
+kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/install-validating-webhook.yaml
+
 cd ./argo-events
 kubectl apply -f project.yaml -n argocd
 kubectl apply -f application.yaml -n argocd
--- install_argo-workflow.sh
+++ install_argo-workflow.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+kubectl apply -n argo-workflow -f https://raw.githubusercontent.com/argoproj/argo-workflows/stable/manifests/namespace-install.yaml 
+
 cd ./argo-workflow
 kubectl apply -f project.yaml -n argocd
 kubectl apply -f application.yaml -n argocd
