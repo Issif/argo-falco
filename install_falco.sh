54cc6a17e111ab09c22ca06cc0ce8d5cc4290f64
Thomas Labarussias <issif_github@gadz.org>
2021-05-09 22:27:42 +0200

    names have been inverted (sic)
    Signed-off-by: Thomas Labarussias <issif_github@gadz.org>

--- install_falco.sh
+++ install_falco.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-kubectl port-forward svc/argocd-server -n argocd 8080:443 &
-
-ARGO_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-echo "Credentials for UI are admin / ${ARGO_PASSWORD}"
+cd ./falco
+kubectl apply -f project.yaml -n argocd
+kubectl apply -f application.yaml -n argocd
