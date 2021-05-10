0c2ee142c7652fbfb370fd914e27ada7d38ad68d
Thomas Labarussias <issif_github@gadz.org>
2021-05-10 21:52:07 +0200

    change namespace

--- install.sh
+++ install.sh
@@ -5,15 +5,16 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 echo " -- Install Argo Events --"
-kubectl create namespace argo
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/install.yaml
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/install-validating-webhook.yaml
-kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo-events/stable/examples/eventbus/native.yaml
+kubectl create namespace argo-events
+kubectl apply -n argo-events -f https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/install.yaml
+kubectl apply -n argo-events -f https://raw.githubusercontent.com/argoproj/argo-events/stable/manifests/install-validating-webhook.yaml
+kubectl apply -n argo-events -f https://raw.githubusercontent.com/argoproj/argo-events/stable/examples/eventbus/native.yaml
 kubectl apply -f argo-events/deployment/project.yaml -n argocd
 kubectl apply -f argo-events/deployment/application.yaml -n argocd
 
 echo "-- Install Argo Worklows --"
-kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo-workflows/stable/manifests/namespace-install.yaml 
+kubectl create namespace argo
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo-workflows/stable/manifests/install.yaml
 kubectl apply -f argo-workflow/deployment/project.yaml -n argocd
 kubectl apply -f argo-workflow/deployment/application.yaml -n argocd
 
@@ -21,10 +22,18 @@ echo "-- Install Falco --"
 kubectl apply -f falco/deployment/project.yaml -n argocd
 kubectl apply -f falco/deployment/application.yaml -n argocd
 
+echo "-- Wait until Pods are up & running --"
+ARGOCD_SERVER=$(kubectl get pods -n argocd | grep argocd-server | cut -f1 -d" ")
+ARGOWORKFLOW_SERVER=$(kubectl get pods -n argo | grep argo-server | cut -f1 -d" ")
+kubectl -n argocd wait pod/${ARGOCD_SERVER} --for=condition=Ready
+kubectl -n argo wait pod/${ARGOWORKFLOW_SERVER} --for=condition=Ready
+
 echo "-- Access to ArgoCD UI --"
 ARGO_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo "Credentials for ArgoCD UI are admin / ${ARGO_PASSWORD}"
-kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
 echo "-- Access to Argo Workflow UI --"
 kubectl port-forward svc/argo-server -n argo 2746:2746 &
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+echo "Link for ArgoCD UI https://localhost:8080"
+echo "Link for Argo Workflow https://localhost:2746"
